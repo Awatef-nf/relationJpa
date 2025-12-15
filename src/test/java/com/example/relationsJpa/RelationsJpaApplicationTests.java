f6a13692b9b561325576051ee1299435d111492a
awatef-toutou <awatefnf@hotmail.fr>
2025-12-15 11:05:20 +0100

    les relations Jpa

---
+++ src/test/java/com/example/relationsJpa/RelationsJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.relationsJpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RelationsJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
